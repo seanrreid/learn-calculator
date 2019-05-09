a48bf6a22e47203e82f51fb80d1db48f39b270db
Sean R Reid <sean@torchcodelab.com>
2019-05-09 13:11:42 -0400

    Added the calculations to the result event handler

--- js/script.js
+++ js/script.js
@@ -57,7 +57,50 @@ Array.from(operators).map(operator => {
 
 // on click of 'equal' button
 result.addEventListener("click", function() {
-    
+    const currentString = input.innerHTML;
+    const numberStringArray = currentString.split(/\+|\-|\*|\//g);
+
+    let numbersArray = [];
+    // Loop through the numberStringArray, and convert the string to int and write to a new array
+    numberStringArray.forEach(function(number){
+        numbersArray.push(Number(number));
+    });
+
+    // Get an array of the operators
+    const operatorsArray = currentString.replace(/[0-9]|\./g, "").split("");
+
+    // We need 4 while loops to do each math operation
+    let multiply = operatorsArray.indexOf("*");
+    while (multiply != -1) {
+        //array.splice(start, deleteCount, value);
+        numbersArray.splice(multiply, 2, numbersArray[multiply] * numbersArray[multiply + 1]);
+        operatorsArray.splice(mutliply, 1);
+        multiply = operatorsArray.indexOf("*");
+    }
+
+    let divide = operatorsArray.indexOf("/");
+    while (divide != -1) {
+        numbersArray.splice(divide, 2, numbersArray[divide] / numbersArray[divide + 1]);
+        operatorsArray.splice(divide, 1);
+        divide = operatorsArray.indexOf("/");
+    }
+
+    let add = operatorsArray.indexOf("+");
+    while (add != -1) {
+        numbersArray.splice(add, 2, numbersArray[add] + numbersArray[add + 1]);
+        operatorsArray.splice(add, 1);
+        add = operatorsArray.indexOf("+");
+    }
+
+    let subtract = operatorsArray.indexOf("-");
+    while (subtract != -1) {
+        numbersArray.splice(subtract, 2, numbersArray[subtract] - numbersArray[subtract + 1]);
+        operatorsArray.splice(subtract, 1);
+        subtract = operatorsArray.indexOf("-");
+    }
+
+    resultDisplayed = true;
+    input.innerHTML = numbersArray;
 });
 
 // clearing the input on press of clear
