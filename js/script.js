e6084f45f689e306617f06905a2e688319016da6
Sean R Reid <sean@torchcodelab.com>
2020-02-05 09:03:05 -0500

    Updating instructions in comments and refactored the attribution

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-"use strict";
+'use strict';
 
-const input = document.getElementById("input"), // input/output button
-    numbers = document.querySelectorAll(".numbers div"), // number buttons
-    operators = document.querySelectorAll(".operators div"), // operator buttons
-    result = document.getElementById("result"), // equal button
-    clear = document.getElementById("clear"); // clear button
+const input = document.getElementById('input'), // input/output button
+  numbers = document.querySelectorAll('.numbers div'), // number buttons
+  operators = document.querySelectorAll('.operators div'), // operator buttons
+  result = document.getElementById('result'), // equal button
+  clear = document.getElementById('clear'); // clear button
 
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
-// numbers is a NodeList object, we need to make it into an array first, then we can map through it...
-
-// adding click handlers to the calculation buttons
+// add click handlers to the calculation buttons
 
 // on click of 'equal' button, perform the mathematical operation
 
